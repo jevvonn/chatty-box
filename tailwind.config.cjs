41bba49dd52e6abd3db49ed0db8da264b9e9e680
jevvonn <darmawanb985@gmail.com>
2023-03-24 22:24:36 +0700

    adding daisy ui

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,24 @@ const config = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#661AE6",
+          secondary: "#6ee7b7",
+          accent: "#1FB2A5",
+          neutral: "#191D24",
+          "base-100": "#2A303C",
+          info: "#3ABFF8",
+          success: "#36D399",
+          warning: "#FBBD23",
+          error: "#F87272",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
 };
 
 module.exports = config;
